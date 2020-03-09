c6055ac7d0dbe44687a776c3240a46ab5d381dd4
kphacko <karan2000patil@gmail.com>
2020-03-09 12:21:26 +0530

    header file created

---
+++ UI/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from aba.zarko-m.com/preview/Default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 05 Mar 2020 04:17:54 GMT -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="zarko-m">
+    <meta name="description" content="Admin Dashboard">
+
+	<title>DAXY | MS</title>
+
+	<!--Bootstrap v4.3.1-->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Font-awesome -->
+	<link rel="stylesheet" href="../../../use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<!--dripicons -->
+	<link href="css/dripicons.css" rel="stylesheet">
+	<!-- fullcalendar -->
+	<link rel="stylesheet" type="text/css" href="css/fullcalendar.min.css">
+	<!-- perfect-scrollbar -->
+	<link rel="stylesheet" type="text/css" href="css/perfect-scrollbar.css">
+	<!-- bootstrap-select -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+	<!-- bootstrap-tour -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-tour-standalone.min.css">
+
+	<!-- Custom -->
+  	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.html" />
+	<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="img/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="../../ms-icon-144x144.html">
+	<meta name="theme-color" content="#ffffff">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
