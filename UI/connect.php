08823c5731d91c168d20f655ba79bd24607a3a42
kphacko <karan2000patil@gmail.com>
2020-03-14 15:54:08 +0530

    connect file testing

--- UI/connect.php
+++ UI/connect.php
@@ -1,9 +1,13 @@
 <?php
-$dbServername= "localhost";
-$dbUsername= 'root';
-$dbPassword='' ;
+$dbServername= "dev.daxy.in:3306";
+$dbUsername= 'test';
+$dbPassword='VzvjkOpRYQ2x9hEg' ;
 $dbName= 'bhajiwaala';
 
+// $dbServername= "localhost";
+// $dbUsername= 'root';
+// $dbPassword='' ;
+// $dbName= 'bhajiwaala';
 
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("Connection failed");
